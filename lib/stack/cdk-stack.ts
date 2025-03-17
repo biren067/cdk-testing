bab14d44e09628c38f88f9e3284df9163bbfb633
anbira <emailing2anjali.com>
2025-03-17 22:37:21 +0530

    added secret-manger for github

--- lib/stack/cdk-stack.ts
+++ lib/stack/cdk-stack.ts
@@ -7,10 +7,13 @@ export class CdkPipelineDemoStack extends cdk.Stack
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope,id,props)
     
+
+    const githubToken = cdk.SecretValue.secretsManager('github-secret-token');
+
     new CodePipeline(this,'Pipeline',{
         pipelineName: 'TestPipeline',
         synth: new ShellStep('Synth',{
-            input: CodePipelineSource.gitHub('biren067/cdk-testing','master'),
+            input: CodePipelineSource.gitHub('biren067/cdk-testing','master',{authentication:githubToken}),
             commands:[
                 'npm ci',
                 'npm run build',
